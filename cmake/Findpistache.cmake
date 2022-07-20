9e2d9ea760ee28146582f8965f7a430757bafd1b
Richard <legalize@xmission.com>
2022-07-20 11:08:59 -0600

    Fix variable name (case sensitive)

--- cmake/Findpistache.cmake
+++ cmake/Findpistache.cmake
@@ -5,7 +5,7 @@ find_file(PISTACHE_INCLUDE pistache)
 
 find_package_handle_standard_args(pistache DEFAULT_MSG PISTACHE_LIB PISTACHE_INCLUDE)
 
-if(pistache_found)
+if(pistache_FOUND)
     get_filename_component(PISTACHE_LIB_DIR ${PISTACHE_LIB} DIRECTORY)
     get_filename_component(PISTACHE_INCLUDE_DIR ${PISTACHE_INCLUDE} DIRECTORY)
     add_library(pistache::pistache SHARED IMPORTED)
